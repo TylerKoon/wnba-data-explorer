4f9edfb5ec3d0af68f9400bee15237004999134d
Tyler Koon <156255467+TylerKoon@users.noreply.github.com>
2025-08-01 01:01:48 +0000

    WIP

--- wnba-data-explorer/src/components/DashboardNavigation.vue
+++ wnba-data-explorer/src/components/DashboardNavigation.vue
@@ -1,31 +1,21 @@
 <script setup lang="ts">
-const categories = {
-    "shooting-trends": {
-        label: "Shooting Trends",
-        icon: "chart-bar",
-        dashboards: [
-            {
-                label: "Player Trends",
-                route: "/dashboard/shooting-trends/player-trends"
-            },
-            {
-                label: "Team Trends",
-                route: "/dashboard/shooting-trends/team-trends"
-            }
-        ]
+defineProps({
+    categories: {
+        type: Object,
+        required: true
     }
-}
+});
 
 </script>
 
 <template>
-    <div class="flex flex-col h-full">
+    <div class="flex flex-col h-full min-w-64">
         <h2 className="text-lg font-bold p-4 bg-red-500">WNBA Data Explorer</h2>
         <ul className="menu bg-base w-full flex-1 bg-red-300">
             <li v-for="category in categories" :key="category.label">
                 <details open>
                     <summary>
-                        <p>{{ category.label }}</p>
+                        {{ category.label }}
                     </summary>
                     <ul>
                         <li v-for="dashboard in category.dashboards" :key="dashboard.label">
--- wnba-data-explorer/src/components/DashboardTableauContainer.vue
+++ wnba-data-explorer/src/components/DashboardTableauContainer.vue
@@ -1,1 +1,18 @@
-<template></template>
+<script setup>
+defineProps({
+  tableauUrl: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <!-- TODO: Maybe add a loading spinner and error page -->
+  <div class="flex h-screen">
+    <tableau-viz id="tableauViz"       
+      src=":tableauUrl"     
+      toolbar="bottom" hide-tabs>
+    </tableau-viz>
+  </div>
+</template>
